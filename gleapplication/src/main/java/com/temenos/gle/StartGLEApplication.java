d31bc20866223f5d543c613535168c16a0aaa0e9
ponmanikandanb <ponmanikandanb@temenos.com>
2019-10-19 13:11:58 +0530

    data soruce application context added

--- gleapplication/src/main/java/com/temenos/gle/StartGLEApplication.java
+++ gleapplication/src/main/java/com/temenos/gle/StartGLEApplication.java
@@ -2,12 +2,16 @@ package com.temenos.gle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class StartGLEApplication {
 
     // start everything
     public static void main(String[] args) {
+    	ApplicationContext context = 
+        		new ClassPathXmlApplicationContext("gle-spring-context.xml");
         SpringApplication.run(StartGLEApplication.class, args);
     }
     
